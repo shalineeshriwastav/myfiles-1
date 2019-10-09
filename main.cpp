ff10a1bedf2e532e80af98cdd4244d6ab8c30235
Saurav Shriwastav <42442539+sauravsvt@users.noreply.github.com>
2019-10-09 10:05:36 +0530

    Add files via upload
    Ascending Order

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    cout<<"Hello sauravsvt!"<<endl;
+    int arr[5]={3,5,1,8,9};
+    for(int i=0;i<5;i++)
+    {
+        for(int j=i+1;j<=4;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int x;
+                x=arr[i];
+                arr[i]=arr[j];
+                arr[j]=x;
+            }
+
+        }
+         cout<<arr[i]<<"\n";
+    }
+
+    return 0;
 }
